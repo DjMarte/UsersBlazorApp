02610203901ddbb76d8b373e40276b3481e5a50e
Levid <jassontejada@gmail.com>
2024-07-08 01:59:58 -0500

    Creé la carpeta Services, con sus servicios

---
+++ UserBlazorApp.API/Services/RoleClaimService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersBlazorApp.Data.Context;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Services;
+
+public class RoleClaimService(UsersDbContext Contexto) : IApiService<AspNetRoleClaims>
+{
+	public async Task<List<AspNetRoleClaims>> GetAllAsync() {
+		return await Contexto.AspNetRoleClaims.Include(r => r.Role).ToListAsync();
+	}
+
+	public async Task<AspNetRoleClaims> GetIdAsync(int id) {
+		return await Contexto.AspNetRoleClaims.FindAsync(id);
+	}
+
+	public async Task<AspNetRoleClaims> AddAsync(AspNetRoleClaims user) {
+		Contexto.AspNetRoleClaims.Add(user);
+		await Contexto.SaveChangesAsync();
+		return user;
+	}
+
+	public async Task<bool> UpdateAsync(AspNetRoleClaims user) {
+		Contexto.Entry(user).State = EntityState.Modified;
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> DeleteAsync(int id) {
+		var usuario = await Contexto.AspNetRoleClaims.FindAsync(id);
+		if (usuario == null) return false;
+
+		Contexto.AspNetRoleClaims.Remove(usuario);
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+}
---
+++ UserBlazorApp.API/Services/UserClaimService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersBlazorApp.Data.Context;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Services;
+
+public class UserClaimService(UsersDbContext Contexto) : IApiService<AspNetUserClaims>
+{
+	public async Task<List<AspNetUserClaims>> GetAllAsync()
+	{
+		return await Contexto.AspNetUserClaims.Include(u => u.UserId).ToListAsync();
+	}
+
+	public async Task<AspNetUserClaims> GetIdAsync(int id)
+	{
+		return await Contexto.AspNetUserClaims.FindAsync(id);
+	}
+
+	public async Task<AspNetUserClaims> AddAsync(AspNetUserClaims userClaim)
+	{
+		Contexto.AspNetUserClaims.Add(userClaim);
+		await Contexto.SaveChangesAsync();
+		return userClaim;
+	}
+
+	public async Task<bool> UpdateAsync(AspNetUserClaims userClaim)
+	{
+		Contexto.Entry(userClaim).State = EntityState.Modified;
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> DeleteAsync(int id)
+	{
+		var userClaim = await Contexto.AspNetUserClaims.FindAsync(id);
+		if (userClaim == null) return false;
+
+		Contexto.AspNetUserClaims.Remove(userClaim);
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+}
