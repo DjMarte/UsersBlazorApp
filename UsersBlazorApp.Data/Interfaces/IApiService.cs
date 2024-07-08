25e8b0ef33746adc1dc996322e6d92d7d3458ea4
Levid <jassontejada@gmail.com>
2024-07-08 17:13:51 -0500

    Creé una interfaz para el API

---
+++ UsersBlazorApp.Data/Interfaces/IApiService.cs
@@ -0,0 +1,14 @@
+﻿namespace UsersBlazorApp.Data.Interfaces;
+
+public interface IApiService<T>
+{
+	public Task<List<T>> GetAllAsync();
+
+	public Task<T> GetIdAsync(int id);
+
+	public Task<T> AddAsync(T entity);
+
+	public Task<bool> UpdateAsync(T entity);
+
+	public Task<bool> DeleteAsync(int id);
+}
