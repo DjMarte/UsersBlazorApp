5b44a40bdf46584653cf93b8c6b52c73efd4697b
Levid <jassontejada@gmail.com>
2024-07-08 17:19:32 -0500

    Agregué los componentes y el servicio, ademas agregué los using en el Import

---
+++ UserBlazorApp.UI/Services/UsuarioService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Json;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+
+public class UsuarioService(HttpClient httpClient)
+{
+	public async Task<List<AspNetUsers>> GetAllAsync() {
+		var resultado = await httpClient.GetAsync("api/AspNetUsers");
+		if (resultado.IsSuccessStatusCode)
+			return await resultado.Content.ReadFromJsonAsync<List<AspNetUsers>>();
+
+		return null;
+	}
+
+	public async Task<AspNetUsers> GetByIdAsync(int id) {
+		var resultado = (await httpClient.GetAsync($"api/AspNetUsers/{id}"))!;
+
+		if (resultado.IsSuccessStatusCode)
+			return await resultado.Content.ReadFromJsonAsync<AspNetUsers>();
+
+		return null!;
+	}
+
+	public async Task<AspNetUsers> CreateAsync(AspNetUsers usuario) {
+		var resultado = await httpClient.PostAsJsonAsync("api/AspNetUsers", usuario);
+
+		if (resultado.IsSuccessStatusCode)
+			return await resultado.Content.ReadFromJsonAsync<AspNetUsers>();
+
+		return null;
+	}
+
+	public async Task<bool> UpdateAsync(AspNetUsers usuario) {
+		var resultado = await httpClient.PutAsJsonAsync($"api/AspNetUsers/{usuario.Id}", usuario);
+		return resultado.IsSuccessStatusCode;
+	}
+
+	public async Task<bool> DeleteAsync(int id) {
+		var resultado = await httpClient.DeleteAsync($"api/AspNetUsers/{id}");
+		return resultado.IsSuccessStatusCode;
+	}
+}
